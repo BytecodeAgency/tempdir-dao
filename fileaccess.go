122ad9b494f4a0b6f43af396251706826ebba7d3
Luciano Nooijen <luciano@bytecode.nl>
2019-11-02 21:38:11 +0100

    Added fileaccess.go
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

---
+++ fileaccess.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"io/ioutil"
+	"math/rand"
+	"os"
+)
+
+const tempDirBase = "./temp/"
+
+type TempFileAccess struct {
+	tempDirLocation string
+}
+
+func generateRandomDirName () string {
+	randomDirNameLength := 16
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	dirName := make([]rune, randomDirNameLength)
+	for i := range dirName {
+		dirName[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(dirName)
+}
+
+func newTempFileAccess () (*TempFileAccess, error) {
+	dirName := generateRandomDirName()
+	tempDirLocation := tempDirBase + dirName + "/"
+	errCreatingDir := os.MkdirAll(tempDirLocation, 0755)
+	return &TempFileAccess{
+		tempDirLocation: tempDirLocation,
+	}, errCreatingDir
+}
+
+func (tfa *TempFileAccess) LoadDirContents (filename string) ([]os.FileInfo, error) {
+	return ioutil.ReadDir(tfa.tempDirLocation)
+}
+
+func (tfa *TempFileAccess) LoadFileContents (filename string) ([]byte, error) {
+	fileLocation := tfa.tempDirLocation + filename
+	return ioutil.ReadFile(fileLocation)
+}
+
+func (tfa *TempFileAccess) SaveFile (filename string, fileContents []byte, permissions os.FileMode) error {
+	fileLocation := tfa.tempDirLocation + filename
+	return ioutil.WriteFile(fileLocation, fileContents, permissions)
+}
+
+func (tfa *TempFileAccess) RemoveTempFileAccess () error {
+	return os.Remove(tfa.tempDirLocation)
+}
+
